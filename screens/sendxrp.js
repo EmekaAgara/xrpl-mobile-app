adb36676492fede961aa2e8036ba73f8569a1f35
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-03-08 11:00:46 +0100

    send xrp screen scroll

--- screens/sendxrp.js
+++ screens/sendxrp.js
@@ -9,8 +9,12 @@ export default class Sendxrp extends PureComponent {
   render() {
     
     return (
-      <SafeAreaView  style={styles.safeArea}>
+      <SafeAreaView
+         
+        style={styles.safeArea}>
         <WebView 
+          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
           source={{ uri: 'https://xrpl-project.netlify.app/' }} 
         />
       </SafeAreaView>
@@ -21,6 +25,9 @@ export default class Sendxrp extends PureComponent {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    backgroundColor: '#000'
+    backgroundColor: '#000',
+    maxWidth:'100%',
+    // max-width: '100%',
+    // overflow-x: hidden,
   }
 })
